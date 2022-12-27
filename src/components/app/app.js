35e4924220222a6446a3f7f8b4d1a207a9758163
Valdo <gkorchun@gmail.com>
2022-12-27 16:12:43 +0200

    Revert all

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,41 +1,11 @@
-import React, { Component } from 'react';
-
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import React, { Component } from "react";
 
 export default class App extends Component {
-
-    state = {
-        selectedPersonId: null
-    }
-
-    onPersonSelected = (id) => {
-        this.setState({
-            selectedPersonId: id
-        })
-    }
-
     render() {
-        const { selectedPersonId } = this.state;
-
         return (
-            <div className="swapi-dev">
-                <Header />
-                <RandomPlanet />
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList
-                            onPersonSelected={ this.onPersonSelected } />
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails
-                            selectedPersonId={ selectedPersonId } />
-                    </div>
-                </div>
+            <div>
+                swapi-dev
             </div>
-        );
+        )
     }
-};
+}
