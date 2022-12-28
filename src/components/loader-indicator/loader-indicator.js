c75f7db8a76cc9cddc40bd8d97955c233ca1b575
Valdo <gkorchun@gmail.com>
2022-12-28 12:31:18 +0200

    Added loading indicator

---
+++ src/components/loader-indicator/loader-indicator.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LoaderIndicator = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-0we5ol1c2gqc">
+            <div className="loadingio-spinner-double-ring-4m0j0nih1wd">
+                <div className="ldio-xrrl5q33v6r">
+                    <div></div>
+                    <div></div>
+                    <div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoaderIndicator;
