ca33495a14b4b4e992eb862da574a21f9dd26a20
Valdo <gkorchun@gmail.com>
2023-01-05 17:55:22 +0200

    Refactor item-details component

--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -1,39 +1,37 @@
-import React, { Component, Children, cloneElement } from 'react';
+import React, { Children, cloneElement } from 'react';
 
 import ApiServices from '../../api-services';
 import Helpers from "../helpers";
 
-export default class ItemDetails extends Component {
+const ItemDetails = (props) => {
+    const helpers = new Helpers(),
+          apiServices = new ApiServices(),
+          { data } = props;
 
-    helpers = new Helpers();
-    apiServices = new ApiServices();
-
-    render() {
-        const { data } = this.props;
-
-        if (!data) {
-            return null;
-        }
-
-        const { id, name } = data;
-
-        return (
-            <div className='item-details card' >
-                <img className='item-image'
-                     src={ this.apiServices.getImgUrl(this.props.name, id) }
-                     onError={ this.helpers.onImageError }
-                     alt='item details image' />
-
-                <div className='card-body' >
-                    <h4>{ name }</h4>
-                    <ul className='list-group list-group-flush' >
-                        { Children.map(
-                            this.props.children,
-                            (child) => cloneElement(child, { data })
-                        ) }
-                    </ul>
-                </div>
-            </div>
-        )
+    if (!data) {
+        return null;
     }
-}
+
+    const { id, name } = data;
+
+    return (
+        <div className='item-details card' >
+            <img className='item-image'
+                 src={ apiServices.getImgUrl(props.name, id) }
+                 onError={ helpers.onImageError }
+                 alt='item details image' />
+
+            <div className='card-body' >
+                <h4>{ name }</h4>
+                <ul className='list-group list-group-flush' >
+                    { Children.map(
+                        props.children,
+                        (child) => cloneElement(child, { data })
+                    ) }
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+export default ItemDetails;
