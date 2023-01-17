745c16a0741301a12ab847d0002a3547848d58ec
Valdo <gkorchun@gmail.com>
2023-01-17 13:22:59 +0200

    Base work with routes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
+import { Link } from "react-router-dom";
+
 const Header = (props) => {
     const { onServiceChange } = props;
 
     return (
         <header className='header d-flex'>
             <h3>
-                <a href='#'>
+                <Link to='/'>
                     Star DB
-                </a>
+                </Link>
             </h3>
             <ul className='d-flex'>
                 <li>
-                    <a href='#'>People</a>
+                    <Link to='/people'>People</Link>
                 </li>
                 <li>
-                    <a href='#'>Planets</a>
+                    <Link to='/planets'>Planets</Link>
                 </li>
                 <li>
-                    <a href='#'>Starships</a>
+                    <Link to='/starships'>Starships</Link>
                 </li>
             </ul>
             <button
